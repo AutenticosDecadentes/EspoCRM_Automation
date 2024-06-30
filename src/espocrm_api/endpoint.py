dd1d1f816eaf03f6d55cfa2aa9d347919f9dcfcc
darwing-21 <darwingarcia2130@gmail.com>
2024-06-30 01:13:06 -0400

    refactor(endpoint): delete enponits

--- src/espocrm_api/endpoint.py
+++ src/espocrm_api/endpoint.py
@@ -10,11 +10,3 @@ class Endpoint(Enum):
     LISTA_EQUIPOS_OFFSET_MAYOR_TOTAL = "/Team?select=name&maxSize=20&offset=201&orderBy=name&order=asc"
     LISTA_EQUIPOS_ORDEN_ASC = "/Team?select=name&maxSize=20&offset=0&orderBy=name&order=asc"
     LISTA_EQUIPOS_ORDEN_DESC = "/Team?select=name&maxSize=20&offset=0&orderBy=name&order=desc"
-
-    LISTA_USUARIOA = "/Usuario?select=name&maxSize=20&offset=0&orderBy=name&order=asc"
-    LISTA_USUARIOA_SIN_SELECT = "/Usuario?maxSize=2&offset=0&order=asc"
-    LISTA_USUARIOA_SELECT_DESCONOCIDO = "/Usuario?select=unknown&maxSize=20&offset=0&orderBy=name&order=asc"
-    LISTA_USUARIOA_MAXSIZE_MAYOR_TOTAL = "/Usuario?select=name&maxSize=200&offset=0&orderBy=name&order=asc"
-    LISTA_USUARIOA_OFFSET_MAYOR_TOTAL = "/Usuario?select=name&maxSize=20&offset=201&orderBy=name&order=asc"
-    LISTA_USUARIOA_ORDEN_ASC = "/Usuario?select=name&maxSize=20&offset=0&orderBy=name&order=asc"
-    LISTA_USUARIOA_ORDEN_DESC = "/Usuario?select=name&maxSize=20&offset=0&orderBy=name&order=desc"

eb9c4a6a5a75ab2331f57e86534237bf41fad4b8
Actodi <diegoacosta9239@gmail.com>
2024-07-01 17:45:36 -0400

    refactor(refactor): add_TestCases

--- test/Usuarios/test_cases.py
+++ test/Usuarios/test_cases.py
@@ -6,58 +6,95 @@ from src.assertions.schema_assertions import AssertionSchemas
 from src.espocrm_api.endpoint import Endpoint
 from src.resources.authentications.authentication import Authentication
 
+
+# Functional
 def test_buscar_usuario_por_nombre_valido(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="darwin"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
+# Functional
 def test_buscar_usuario_por_apellido_encontrado(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="Montano"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
+# Functional
 def test_buscar_usuario_por_username_encontrado(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="hola"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
+# Functional
 def test_buscar_usuario_por_email_encontrado(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="201604461@est.umss"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
-def test_buscar_usuario_por_email_encontrado(get_headers):
+# Functional
+def test_buscar_usuario_por_email_invalido(get_headers): # Corregido: email inválido, no encontrado
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="mnbmbmbjkhghjjjg"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
-def test_buscar_usuario_por_email_encontrado(get_headers):
+# Functional
+def test_buscar_usuario_por_apellido_no_valido(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="apellido_no_valido"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
-def test_buscar_usuario_por_email_encontrado(get_headers):
-    response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="username_no_valido"), 'GET')
-    AssertionSchemas().assert_status_code(response, 200)
-    AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
-
+# Functional
 def test_buscar_usuario_por_username_no_valido(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="username_no_valido"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
+# Functional
 def test_buscar_usuario_por_email_no_valido(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="email_no_valido"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
+# Functional
 def test_buscar_usuario_campo_vacio(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where=""), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
 
-
+# Functional, Smoke
 def test_buscar_usuario_con_login_invalido(get_headers):
-    response = Authentication().authenticate_invalid_user(get_headers,  Endpoint.BUSCAR_USUARIOS(where="diego"), 'GET')
+    response = Authentication().authenticate_invalid_user(get_headers, Endpoint.BUSCAR_USUARIOS(where="diego"), 'GET')
     AssertionSchemas().assert_status_code(response, 401)
     AssertionSchemas().assert_response_vacio(response.text)
+
+# Functional, Regression
+def test_buscar_usuarios_con_maxSize(get_headers):
+    response = Authentication().authenticate_valid_user(
+        get_headers, Endpoint.BUSCAR_USUARIOS(maxSize=5), 'GET'
+    )
+    AssertionSchemas().assert_status_code(response, 200)
+    AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
+
+# Functional, Regression
+def test_buscar_usuarios_con_offset(get_headers):
+    response = Authentication().authenticate_valid_user(
+        get_headers, Endpoint.BUSCAR_USUARIOS(offset=10), 'GET'
+    )
+    AssertionSchemas().assert_status_code(response, 200)
+    AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
+
+# Functional, Regression
+def test_buscar_usuarios_con_orderBy_nombre(get_headers):
+    response = Authentication().authenticate_valid_user(
+        get_headers, Endpoint.BUSCAR_USUARIOS(orderBy="name"), 'GET'
+    )
+    AssertionSchemas().assert_status_code(response, 200)
+    AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())
+
+# Functional, Regression
+def test_buscar_usuarios_con_order_desc(get_headers):
+    response = Authentication().authenticate_valid_user(
+        get_headers, Endpoint.BUSCAR_USUARIOS(order="desc"), 'GET'
+    )
+    AssertionSchemas().assert_status_code(response, 200)
+    AssertionSchemas().assert_usuarios_buscar_schema_file(response.json())

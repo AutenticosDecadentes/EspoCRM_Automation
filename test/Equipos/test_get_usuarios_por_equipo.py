dd62e7d969783e3c8f78343e035a70fbac20f6a8
darwing-21 <darwingarcia2130@gmail.com>
2024-07-01 16:44:58 -0400

    feat(mark): add mark a test cases

--- test/Equipos/test_get_usuarios_por_equipo.py
+++ test/Equipos/test_get_usuarios_por_equipo.py
@@ -6,48 +6,66 @@ from src.espocrm_api.endpoint import Endpoint
 from src.resources.authentications.authentication import Authentication
 
 
+@pytest.mark.regression
+@pytest.mark.functional
+@pytest.mark.smoke
 def test_datos_por_defecto(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_equipo_usuarios_schema_file(response.json())
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_max_size0(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(maxSize=0), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_equipo_usuarios_schema_file(response.json())
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_max_size_alto_mas_de_200(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(maxSize=201), 'GET')
     AssertionSchemas().assert_status_code(response, 403)
     AssertionSchemas().assert_response_vacio(response.text)
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_max_size_alto_menos_de_200(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(maxSize=195), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_equipo_usuarios_schema_file(response.json())
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_max_size_negativo(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(maxSize=-15), 'GET')
     AssertionSchemas().assert_status_code(response, 500)
     AssertionSchemas().assert_response_vacio(response.text)
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_max_size_negativo(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(maxSize="hola"), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_equipo_usuarios_schema_file(response.json())
 
 
+@pytest.mark.regression
+@pytest.mark.functional
+@pytest.mark.smoke
 def test_lista_equipos_usuarios_autenficacion_invalida(get_headers):
     response = Authentication().authenticate_invalid_user(get_headers, Endpoint.EQUIPO_USUARIOS(), 'GET')
     AssertionSchemas().assert_status_code(response, 401)
     AssertionSchemas().assert_response_vacio(response.text)
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_lista_equipos_usuarios_orden_asc(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(order='asc'), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
@@ -55,6 +73,8 @@ def test_lista_equipos_usuarios_orden_asc(get_headers):
     AssertionUsuarios().assert_check_orden_asc(response.json())
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_lista_equipos_usuarios_orden_desc(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(order='desc'), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
@@ -62,12 +82,16 @@ def test_lista_equipos_usuarios_orden_desc(get_headers):
     AssertionUsuarios().assert_check_orden_desc(response.json())
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_lista_equipos_usuarios_ordenBy_vacio(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(orderBy=''), 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_equipo_usuarios_schema_file(response.json())
 
 
+@pytest.mark.regression
+@pytest.mark.functional
 def test_lista_equipos_usuarios_ordenBy_null(get_headers):
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.EQUIPO_USUARIOS(orderBy="null"), 'GET')
     AssertionSchemas().assert_status_code(response, 400)
--- test/Equipos/test_ver_equipo.py
+++ test/Equipos/test_ver_equipo.py
@@ -6,7 +6,10 @@ from src.resources.authentications.authentication import Authentication
 from src.espocrm_api.api_request import EspocrmRequest
 import requests
 
-def test_ver_equipo_headers_adicionales(get_headers): #52
+
+@pytest.mark.regression
+@pytest.mark.functional
+def test_ver_equipo_headers_adicionales(get_headers):  # 52
     url = f'https://espo.spartan-soft.com/api/v1{Endpoint.VER_EQUIPOS.value}'
     headers = get_headers("admin", "admin")
     headers["header_extra"] = "valor_extra"
@@ -16,31 +19,49 @@ def test_ver_equipo_headers_adicionales(get_headers): #52
     AssertionSchemas().assert_ver_equipo_schema_file(response.json())
 
 
-
-def test_ver_equipo_metodo_http_incorrecto(get_headers): #51
+@pytest.mark.regression
+@pytest.mark.functional
+def test_ver_equipo_metodo_http_incorrecto(get_headers):  # 51
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.VER_EQUIPOS.value, 'POST')
     AssertionSchemas().assert_status_code(response, 405)
 
 
-def test_ver_equipo_correcto(get_headers): #46 revisar
+@pytest.mark.regression
+@pytest.mark.functional
+@pytest.mark.smoke
+def test_ver_equipo_correcto(get_headers):  # 46 revisar
     response = Authentication().authenticate_valid_user(get_headers, Endpoint.VER_EQUIPOS.value, 'GET')
     AssertionSchemas().assert_status_code(response, 200)
     AssertionSchemas().assert_ver_equipo_schema_file(response.json())
 
 
-
-def test_ver_equipo_autorizacion_incorrecta(get_headers): #48
+@pytest.mark.regression
+@pytest.mark.functional
+def test_ver_equipo_autorizacion_incorrecta(get_headers):  # 48
     response = Authentication().authenticate_invalid_user(get_headers, Endpoint.VER_EQUIPOS.value, 'GET')
     AssertionSchemas().assert_status_code(response, 401)
 
-def test_ver_equipo_sin_autorizacion(get_headers): #47
+
+@pytest.mark.regression
+@pytest.mark.functional
+def test_ver_equipo_sin_autorizacion(get_headers):  # 47
     response = Authentication().authenticate_user_empty(get_headers, Endpoint.VER_EQUIPOS.value, 'GET')
     AssertionSchemas().assert_status_code(response, 401)
 
-def test_ver_equipo_id_no_existe(get_headers): #49
-    response = Authentication().authenticate_valid_user(get_headers, 'https://espo.spartan-soft.com/api/v1/Team/id_no_existente', 'GET')
+
+@pytest.mark.regression
+@pytest.mark.functional
+def test_ver_equipo_id_no_existe(get_headers):  # 49
+    response = Authentication().authenticate_valid_user(get_headers,
+                                                        'https://espo.spartan-soft.com/api/v1/Team/id_no_existente',
+                                                        'GET')
     AssertionSchemas().assert_status_code(response, 404)
 
-def test_ver_equipo_id_formato_incorrecto(get_headers): #
-    response = Authentication().authenticate_valid_user(get_headers, 'https://espo.spartan-soft.com/api/v1/Team/formatoIncorrecto', 'GET')
+
+@pytest.mark.regression
+@pytest.mark.functional
+def test_ver_equipo_id_formato_incorrecto(get_headers):  #
+    response = Authentication().authenticate_valid_user(get_headers,
+                                                        'https://espo.spartan-soft.com/api/v1/Team/formatoIncorrecto',
+                                                        'GET')
     AssertionSchemas().assert_status_code(response, 404)

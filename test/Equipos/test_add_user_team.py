98203f8f16cf90455fab4b181e58dc6ebbc0e2c4
MiguelLopez004 <heredero0409@gmail.com>
2024-07-10 10:25:10 -0400

    aaaa

--- test/Equipos/test_add_user_team.py
+++ test/Equipos/test_add_user_team.py
@@ -40,17 +40,17 @@ def test_add_team_multi_users(setup_team_add_user):
     AssertionTeams().assert_users_in_team([user1['id'], user2['id']], team_users)
 
 
-# @pytest.mark.regression
-# @pytest.mark.functional
-# @pytest.mark.smoke
-# def test_add_team_user_invalid_authentication(get_headers, setup_team_add_user):
-#     headers, team, user1, user2 = setup_team_add_user
-#     headers = Auth().get_invalid_user_headers(get_headers)
-#     payload = PayloadTeam().build_payload_add_user_team([user1['id'], user2['id']])
-#     AssertionSchemas().assert_team_add_user_schema_payload_file(payload)
-#     response = EspocrmRequest().post(EndpointTeams.add_users(team['id']), headers, payload)
-#     AssertionStatusCode().assert_status_code_401(response)
-#     AssertionTeams().assert_response_empty(response.text)
+@pytest.mark.regression
+@pytest.mark.functional
+@pytest.mark.smoke
+def test_add_team_user_invalid_authentication(get_headers, setup_team_add_user):
+    headers, team, user1, user2 = setup_team_add_user
+    headers1 = Auth().get_invalid_user_headers(get_headers)
+    payload = PayloadTeam().build_payload_add_user_team([user1['id'], user2['id']])
+    AssertionSchemas().assert_team_add_user_schema_payload_file(payload)
+    response = EspocrmRequest().post(EndpointTeams.add_users(team['id']), headers1, payload)
+    AssertionStatusCode().assert_status_code_401(response)
+    AssertionTeams().assert_response_empty(response.text)
 
 
 @pytest.mark.regression

36a36e8c64396546625946bc9b888c57432a0f2a
ViancaMamaniRamos <viancamamaniramos@gmail.com>
2024-07-09 21:32:12 -0400

    test case de post completos

--- test/Usuarios/test_add_avatar.py
+++ test/Usuarios/test_add_avatar.py
@@ -9,14 +9,16 @@ from src.assertions.schema_assertions import AssertionSchemas
 
 
 @pytest.mark.smoke
+@pytest.mark.functional
 def test_add_avatar_file_valid_image(setup_add_avatar):
     headers, user = setup_add_avatar
     payload = PayloadAvatar().build_payload_add_avatar(Imagen.imagen_avatar.value)
     response = EspocrmRequest().post(EndpointUsers.avatar(), headers, payload)
     AssertionStatusCode().assert_status_code_200(response)
     AssertionSchemas().assert_add_avatar_schema_file(response.json())
-   
+    
 @pytest.mark.smoke
+@pytest.mark.functional
 def test_add_avatar_file_invalid_image(setup_add_avatar):
     headers, user = setup_add_avatar
     payload = PayloadAvatar().build_payload_add_avatar(Imagen.imagenInvalida.value )
--- test/Usuarios/test_create_user.py
+++ test/Usuarios/test_create_user.py
@@ -6,6 +6,7 @@ from src.assertions.status_code_assertions import AssertionStatusCode
 from src.assertions.schema_assertions import AssertionSchemas
 
 
+@pytest.mark.smoke
 @pytest.mark.functional
 def test_add_invalid_userName(setup_add_user):
     headers, created_users = setup_add_user
@@ -13,7 +14,8 @@ def test_add_invalid_userName(setup_add_user):
     response = EspocrmRequest().post(EndpointUsers.user(), headers, payload)
     AssertionStatusCode().assert_status_code_400(response)
 
-@pytest.mark.smoke
+@pytest.mark.functional
+@pytest.mark.functional
 def test_add_user_without_password(setup_add_user):
      headers, created_users = setup_add_user
      payload = PayloadUser().build_payload_add_user(userName="noname", salutationName="Mrs.", firstName="apellido", lastName="apellidio")
@@ -22,16 +24,17 @@ def test_add_user_without_password(setup_add_user):
      created_user = response.json()
      created_users.append(created_user)
 
-@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.functional
 def test_add_user_duplicate_userName(setup_add_user):
     headers, created_users = setup_add_user
-    payload = PayloadUser().build_payload_add_user(userName = "estrellita11",
+    payload = PayloadUser().build_payload_add_user(userName = "Adrianita",
     type = "admin",
     salutationName = "Mr.",
-    firstName = "vadria",
-    lastName = "sacfvs",
+    firstName = "Adridri",
+    lastName = "Zuna",
     isActive = True,
-    emailAddress = "vianvfver00@gmail.com",
+    emailAddress = "adriadri00@gmail.com",
     phoneNumber = "+59172256555",
     gender = "Male",
     rolesIds = "66758ef1d775a381e",
@@ -53,14 +56,16 @@ def test_add_user_duplicate_userName(setup_add_user):
     AssertionStatusCode().assert_status_code_409(response1)
 
 
+@pytest.mark.smoke
+@pytest.mark.regression
 @pytest.mark.functional
 def test_add_user_with_valid_complete_data(setup_add_user):
     headers, created_users = setup_add_user
     payload = PayloadUser().build_payload_add_user(userName = "vianquis",
     type = "admin",
     salutationName = "Mr.",
-    firstName = "vianquisuwu",
-    lastName = "Marrrrrrrr",
+    firstName = "vimara",
+    lastName = "mara",
     isActive = True,
     emailAddress = "vianquis00@gmail.com",
     phoneNumber = "+59172255555",
@@ -78,4 +83,84 @@ def test_add_user_with_valid_complete_data(setup_add_user):
     AssertionStatusCode().assert_status_code_200(response)
     created_user = response.json()
     created_users.append(created_user)
+
+    
+@pytest.mark.regression
+@pytest.mark.functional
+def test_add_user_empty_email_(setup_add_user):
+    headers, created_users = setup_add_user
+    payload = PayloadUser().build_payload_add_user(
+        userName="usuarioNuevo",
+        type="admin",
+        salutationName="Mr.",
+        firstName="Pedro",
+        lastName="Martinez",
+        isActive=True,
+        emailAddress="",  
+        phoneNumber="+59172256555",
+        gender="Male",
+        rolesIds="66758ef1d775a381e",
+        rolesNames={"66758ef1d775a381e": "Administrador"},
+        workingTimeCalendarId="667594aa8582445d8",
+        workingTimeCalendarName="Calendar",
+        layoutSetId="6675949a021ad4859",
+        layoutSetName="Layout 1",
+        password="securePassword123",
+        passwordConfirm="securePassword123"
+    )
+    response = EspocrmRequest().post(EndpointUsers.user(), headers, payload)
+    AssertionStatusCode().assert_status_code_400(response) 
+
+@pytest.mark.regression
+@pytest.mark.functional
+def test_add_user_short_password(setup_add_user):
+    headers, created_users = setup_add_user
+    payload = PayloadUser().build_payload_add_user(
+        userName="nuevoUsuario",
+        type="admin",
+        salutationName="Mr.",
+        firstName="Carlos",
+        lastName="Gomez",
+        isActive=True,
+        emailAddress="carlos.gomez@egmail.com",
+        phoneNumber="+59172256555",
+        gender="Male",
+        rolesIds="66758ef1d775a381e",
+        rolesNames={"66758ef1d775a381e": "Administrador"},
+        workingTimeCalendarId="667594aa8582445d8",
+        workingTimeCalendarName="Calendar",
+        layoutSetId="6675949a021ad4859",
+        layoutSetName="Layout 1",
+        password="123",  
+        passwordConfirm="123"
+    )
+    response = EspocrmRequest().post(EndpointUsers.user(), headers, payload)
+    print(payload)  
+    AssertionStatusCode().assert_status_code_400(response) 
+
+@pytest.mark.regression
+@pytest.mark.functional
+def test_add_user_invalid_email_format(setup_add_user):
+    headers, created_users = setup_add_user
+    payload = PayloadUser().build_payload_add_user(
+        userName="usuarioNuevo1",
+        type="admin",
+        salutationName="Mr.",
+        firstName="Pedro",
+        lastName="Martinez",
+        isActive=True,
+        emailAddress="email-no-valido",  
+        phoneNumber="+59172256555",
+        gender="Male",
+        rolesIds="66758ef1d775a381e",
+        rolesNames={"66758ef1d775a381e": "Administrador"},
+        workingTimeCalendarId="667594aa8582445d8",
+        workingTimeCalendarName="Calendar",
+        layoutSetId="6675949a021ad4859",
+        layoutSetName="Layout 1",
+        password="securePassword123",
+        passwordConfirm="securePassword123"
+    )
+    response = EspocrmRequest().post(EndpointUsers.user(), headers, payload)
+    AssertionStatusCode().assert_status_code_400(response)  
 
